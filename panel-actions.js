e64bf9329f92312b1f88a7a92746de887ecd5acb
khhdbhhe-cloud <khhdbhhe@gmail.com>
2026-08-25 13:38:57 +0330

    Update panel-actions.js

--- panel-actions.js
+++ panel-actions.js
@@ -1,1 +1,82 @@
+const { Markup } = require("telegraf");
+const { checkAdmin } = require("./security");
 
+
+function registerPanelActions(bot) {
+
+
+  bot.action(
+    /^users:(\d+)$/,
+    async ctx => {
+
+      const access =
+        await checkAdmin(ctx);
+
+      if(!access.ok){
+        return ctx.answerCbQuery(
+          access.text,
+          { show_alert:true }
+        );
+      }
+
+
+      await ctx.answerCbQuery();
+
+
+      await ctx.editMessageText(
+`『𓆩 مدیریت کاربران 𓆪』
+
+بخش مدیریت کاربران فعال شد.
+
+به زودی:
+• بن
+• میوت
+• اخطار
+• شناسنامه
+• آمار`,
+        Markup.inlineKeyboard([
+          [
+            Markup.button.callback(
+              "『𓆩 بازگشت 𓆪』",
+              `back:${ctx.from.id}`
+            )
+          ]
+        ])
+      );
+
+    }
+  );
+
+
+
+  bot.action(
+    /^settings:(\d+)$/,
+    async ctx => {
+
+      await ctx.answerCbQuery();
+
+
+      await ctx.editMessageText(
+`『𓆩 تنظیمات 𓆪』
+
+تنظیمات گروه اینجا قرار می‌گیرد.`,
+        Markup.inlineKeyboard([
+          [
+            Markup.button.callback(
+              "『𓆩 بازگشت 𓆪』",
+              `back:${ctx.from.id}`
+            )
+          ]
+        ])
+      );
+
+    }
+  );
+
+
+}
+
+
+module.exports = {
+  registerPanelActions
+};
